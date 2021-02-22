9e3f6b10e07f064d16c4972e4c0d0b81d2691466
Francis Nguyen <francishtknguyen@gmail.com>
2021-02-22 17:47:45 -0600

    tasks 1-4

--- index.js
+++ index.js
@@ -28,8 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+  the count variable in 1 is confined within the counterMaker function while in counter 2 the count variable is a global variable.
   
   2. Which of the two uses a closure? How can you tell?
+
+  counter1 uses a closure because it has a nested function within the function.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better? 
@@ -65,8 +69,10 @@ NOTE: This will be a callback function for the tasks below
 */
 
 function inning(/*Code Here*/){
-    /*Code Here*/
+    return Math.round(Math.random() * 2);
 }
+
+console.log(inning());
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -83,17 +89,28 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(func, innings){
   /*Code Here*/
+  let Home = 0;
+  let Away = 0;
+  for(let i = 0; i < innings; i++){
+    Home = Home + func();
+    Away = Away + func(); 
+  }
+  return {"Home": Home,"Away": Away};
 }
+console.log(finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
+function getInningScore(func) {
   /*Your Code Here */
+
+  return {"Home": func(), "Away": func()};
+
 }
 
 
@@ -138,11 +155,20 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
+function scoreboard(func4,func2,innings) {
   /* CODE HERE */
+  let arr = [];
+  let Home = 0;
+  let Away = 0;
+  for(let i = 0; i < innings; i++){
+    Home = func4(func2()):
+    Away = func4(func2());
+    return arr.push({`Inning ${i}` : `Away ${Away}` - `Home ${Home}` });
+  }
+  return arr;
 }
 
-
+console.log(scoreboard(getInningScore,inning, 9))
 
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
